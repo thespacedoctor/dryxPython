7950e3fd630d9388bd06403183703b8d5f39c5ff
thespacedoctor <d.r.young@qub.ac.uk>
2013-09-19 15:32:26 +0100

    added a MMD module that uses Fletcher Penny's MMD binary to convert md files to HTML

---
+++ dryxPython/htmlframework/tests/test_htmlframework.py
@@ -0,0 +1,68 @@
+import os
+import nose
+from .. import htmlframework
+
+## SETUP AND TEARDOWN FIXTURE FUNCTIONS FOR THE ENTIRE MODULE
+def setUpModule():
+    import logging
+    import logging.config
+    import yaml
+
+    "set up test fixtures"
+    moduleDirectory = os.path.dirname(__file__) + "/tests"
+
+    # SETUP PATHS TO COMMONG DIRECTORIES FOR TEST DATA
+    global pathToInputDataDir, pathToOutputDir, pathToOutputDataDir, pathToInputDir
+    pathToInputDir = moduleDirectory+"/input/"
+    pathToInputDataDir = pathToInputDir + "data/"
+    pathToOutputDir = moduleDirectory+"/output/"
+    pathToOutputDataDir = pathToOutputDir+"data/"
+
+    # SETUP THE TEST LOG FILE
+    global testlog
+    testlog = open(pathToOutputDir + "tests.log", 'w')
+
+    # SETUP LOGGING
+    loggerConfig = """
+    version: 1
+    formatters:
+        file_style:
+            format: '* %(asctime)s - %(name)s - %(levelname)s (%(filename)s > %(funcName)s > %(lineno)d) - %(message)s  '
+            datefmt: '%Y/%m/%d %H:%M:%S'
+        console_style:
+            format: '* %(asctime)s - %(levelname)s: %(filename)s:%(funcName)s:%(lineno)d > %(message)s'
+            datefmt: '%H:%M:%S'
+        html_style:
+            format: '<div id="row" class="%(levelname)s"><span class="date">%(asctime)s</span>   <span class="label">file:</span><span class="filename">%(filename)s</span>   <span class="label">method:</span><span class="funcName">%(funcName)s</span>   <span class="label">line#:</span><span class="lineno">%(lineno)d</span> <span class="pathname">%(pathname)s</span>  <div class="right"><span class="message">%(message)s</span><span class="levelname">%(levelname)s</span></div></div>'
+            datefmt: '%Y-%m-%d <span class= "time">%H:%M <span class= "seconds">%Ss</span></span>'
+    handlers:
+        console:
+            class: logging.StreamHandler
+            level: DEBUG
+            formatter: console_style
+            stream: ext://sys.stdout
+    root:
+        level: DEBUG
+        handlers: [console]"""
+
+    logging.config.dictConfig(yaml.load(loggerConfig))
+    global log
+    log = logging.getLogger(__name__)
+
+    # x-setup-dbconn-for-test-module
+
+    return None
+
+def tearDownModule():
+    "tear down test fixtures"
+    # CLOSE THE TEST LOG FILE
+    testlog.close()
+    return None
+
+class emptyLogger:
+    info=None
+    error=None
+    debug=None
+    critical=None
+    warning=None
+
---
+++ dryxPython/tests/input/test_file_for_mmd_module.md
@@ -0,0 +1,48 @@
+Filename: `test_file_for_mmd_module`
+
+
+
+# SN Survey Simulation Results - 20130919t134637
+
+The *g*-band liming magnitudes of this simulated survey are:
+
+| Filter | Magnitude |
+|:---|:----|
+| i | 23.3 |
+| r | 23.3 |
+| z | 21.7 |
+| g | 23.3 |
+
+
+A total of **200** PISNe where simulated in the survey, out to a **redshift of 1.0**. A constant galactic extinction of `E(B-V) = 0.023` is used (the mean extinction of the 10 PS1 Medimum Deep Fields). The MDFs are visible for a fraction of 0.5 of the survey year and the typical fraction of survey time lost to weather of 0.4 is accounted for. Here are the relative rates and peak magnitude distributions of the SN used in the survey:
+
+| SN Type | Relative Rate | Peak Magnitude | Sigma Peak |
+|:---|:---|:---|:---|
+| SN2007bi | 0.5 | -17.08 | 0.001 |
+| SLSN | 0.5 | -20.21 | 0.001 |
+
+
+If a PISN is detected by the simulated survey, extra constraints are placed upon the detected object to secure positive identification as a PISN.
+
+1. The peak apparent magnitude of the object must be brighter than **21.5 mag**
+2. The object must be detectable for long enough to complete a follow-up campaign of longer than **100 days** within 1 survey year with any single filter.
+
+The PISN rate for the survey volume is estimated by assuming a rate of **10e-5** times that of the CCSN rate (itself a fraction of **0.007** of the total SFR).
+
+
+## Results ##
+
+This simulated survey discovered a total of **1** PISNe per year. An extra **5** PISNe were detected but deemed too faint to constrain a positive PISN identification and a further **1** PISNe where detected but an observational campaign of more than **100** days could not be completed to ensure identification. See below for the various output plots.
+
+        
+![Cadence_Wheel_for_Simulated_Survey_plot](/Users/Dave/Desktop/qubits_output//plots/Cadence_Wheel_for_Simulated_Survey.png)
+
+
+![PISNe_Detected_Within_the_Suvrey_Year_plot](/Users/Dave/Desktop/qubits_output//plots/PISNe_Detected_Within_the_Suvrey_Year.png)
+
+
+![Redshift_Map_of_PISNe_Simulated_within_the_Survey_Volume_plot](/Users/Dave/Desktop/qubits_output//plots/Redshift_Map_of_PISNe_Simulated_within_the_Survey_Volume.png)
+
+
+![PS1_PISN_Survey___Relative_Detected_Rates_plot](/Users/Dave/Desktop/qubits_output//plots/PS1_PISN_Survey___Relative_Detected_Rates.png)
+
