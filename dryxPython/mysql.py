a042ce486f264e420647abd5174728c3a9155048
thespacedoctor <d.r.young@qub.ac.uk>
2013-10-15 15:35:21 +0100

    small bug fix

--- dryxPython/mysql.py
+++ dryxPython/mysql.py
@@ -118,8 +118,6 @@ def execute_mysql_write_query(
 
     log.info('starting execute_mysql_write_query')
 
-    print sqlQuery + '\n\n'
-
     try:
         cursor = dbConn.cursor(MySQLdb.cursors.DictCursor)
     except Exception, e:
