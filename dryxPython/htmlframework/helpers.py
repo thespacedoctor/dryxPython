07f60eb79c3cd768bd46af80123647fda547fc88
thespacedoctor <nothingbutdave@gmail.com>
2014-06-27 12:25:56 +0100

    x

--- dryxPython/htmlframework/helpers.py
+++ dryxPython/htmlframework/helpers.py
@@ -66,6 +66,60 @@ def unescape_html(html):
 
     return html
 
+## LAST MODIFIED : June 26, 2014
+## CREATED : June 26, 2014
+## AUTHOR : DRYX
+# copy usage method(s) into function below and select the following snippet from the command palette:
+# x-setup-worker-function-parameters-from-usage-method
+def hide_from_device(
+        content="",
+        onPhone=True,
+        onTablet=True,
+        onDesktop=True):
+    """hide from device)
+
+    **Key Arguments:**
+        
+        # copy usage method(s) here and select the following snippet from the command palette:
+        # x-setup-docstring-keys-from-selected-usage-options
+
+    **Return:**
+        - None
+
+    **Todo**
+        - @review: when complete, clean hide_from_device) function
+        - @review: when complete add logging
+        - @review: when complete, decide whether to abstract function to another module
+    """
+    
+    phoneClass = ""
+    tabletClass = ""
+    desktopClass = ""
+    if onPhone:
+        if onTablet:
+            if not onDesktop:
+                desktopClass = "hidden-desktop"
+        else:
+            if not onDesktop:
+                phoneClass = "visible-phone"
+            else:
+                tabletClass = "hidden-tablet"
+    else:
+        if onTablet:
+            if not onDesktop:
+                tabletClass = "visible-tablet"
+            else:
+                phoneClass = "hidden-phone"
+        else:
+            desktopClass = "visible-desktop"
+
+
+    span = """<span class="%(phoneClass)s %(tabletClass)s %(desktopClass)s">%(content)s</span>""" % locals() 
+    return span
+
+# use the tab-trigger below for new function
+# xt-def-with-logger
+
 ###################################################################
 # PRIVATE (HELPER) FUNCTIONS                                      #
 ###################################################################
