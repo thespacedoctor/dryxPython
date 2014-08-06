290c277573dbf92d4e14fa42a5f0ba34e81dcabd
thespacedoctor <nothingbutdave@gmail.com>
2014-08-06 19:25:00 +0100

    x

--- dryxPython/git/tests/test_update_request_watcher.py
+++ dryxPython/git/tests/test_update_request_watcher.py
@@ -4,17 +4,18 @@ import shutil
 from .. import update_request_watcher
 from dryxPython.utKit import utKit
 
-## SETUP AND TEARDOWN FIXTURE FUNCTIONS FOR THE ENTIRE MODULE
+# SETUP AND TEARDOWN FIXTURE FUNCTIONS FOR THE ENTIRE MODULE
 moduleDirectory = os.path.dirname(__file__)
 utKit = utKit(moduleDirectory)
 log, dbConn, pathToInputDir, pathToOutputDir = utKit.setupModule()
 utKit.tearDownModule()
 
+
 class test_command_line():
 
     def test_command_line_method_01(self):
         kwargs = {}
-        kwargs["pathToGitRepos"] = "/Users/Dave/Dropbox/git_repos"
+        kwargs["pathToGitRepos"] = "/Users/Dave/git_repos"
         update_request_watcher.main(kwargs)
 
     # x-class-method-to-test-a-command-line-usage
