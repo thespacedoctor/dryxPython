f83b8c1a5fe255ca262aef348c303a279942f276
thespacedoctor <nothingbutdave@gmail.com>
2014-08-08 09:22:14 +0100

    x

--- dryxPython/htmlframework/forms.py
+++ dryxPython/htmlframework/forms.py
@@ -302,39 +302,39 @@ def formInput(
     appendClass = False
 
     if hidden:
-        hidden = """hidden"""
+        hidden = u"""hidden"""
     else:
-        hidden = ""
+        hidden = u""
 
     falseList = [searchBar, span, prepend, prependContent, append,
                  appendContent, inputId, pull, htmlId, appendClass, prependClass]
 
     for i in range(len(falseList)):
         if not falseList[i]:
-            falseList[i] = ""
+            falseList[i] = u""
 
     [searchBar, span, prepend, prependContent, append, appendContent,
         inputId, pull, htmlId, appendClass, prependClass] = falseList
 
     if pull:
-        pull = "pull-%(pull)s" % locals()
+        pull = u"pull-%(pull)s" % locals()
 
     if span:
-        span = "span%(span)s" % locals()
+        span = u"span%(span)s" % locals()
 
     if searchBar:
-        searchClass = "search-query"
+        searchClass = u"search-query"
     else:
-        searchClass = ""
+        searchClass = u""
 
     if prepend:
-        prependClass = "input-prepend"
-        prependContent = """<span class="add-on">%(prepend)s</span>""" % locals(
+        prependClass = u"input-prepend"
+        prependContent = u"""<span class="add-on">%(prepend)s</span>""" % locals(
         )
 
     if append:
-        appendClass = "input-append"
-        appendContent = """<span class="add-on">%(append)s</span>""" % locals()
+        appendClass = u"input-append"
+        appendContent = u"""<span class="add-on">%(append)s</span>""" % locals()
 
     # if prepend:
     #     if append:
@@ -345,90 +345,91 @@ def formInput(
     #     inputId = "appendedInput "
 
     if button1:
-        appendClass = "input-append"
+        appendClass = u"input-append"
         appendContent = button1
         # inputId = "appendedInputButton "
 
     if button2:
-        appendClass = "input-append"
-        appendContent = """%(appendContent)s %(button2)s""" % locals()
+        appendClass = u"input-append"
+        appendContent = u"""%(appendContent)s %(button2)s""" % locals()
         # inputId = "appendedInputButtons "
 
     if appendDropdown:
-        appendClass = "input-append"
+        appendClass = u"input-append"
         # inputId = "appendedDropdownButton "
-        appendContent = """
+        appendContent = u"""
         <div class="btn-group">
             %(appendDropdown)s
         </div>""" % locals()
 
     if prependDropdown:
-        prependClass = "input-prepend"
+        prependClass = u"input-prepend"
         # inputId = "prependedDropdownButton "
-        prependContent = """
+        prependContent = u"""
         <div class="btn-group">
             %(prependDropdown)s
         </div>""" % locals()
 
-    step = ""
+    step = u""
     if ttype == "float":
-        step = """ step="any" """
-        ttype = "number"
+        step = u""" step="any" """
+        ttype = u"number"
 
     if prependDropdown and appendDropdown:
         pass
         # inputId = "appendedPrependedDropdownButton "
 
     if inlineHelpText:
-        inlineHelpText = """<span class="help-inline">%(inlineHelpText)s</span>""" % locals(
+        inlineHelpText = u"""<span class="help-inline">%(inlineHelpText)s</span>""" % locals(
         )
     else:
-        inlineHelpText = ""
+        inlineHelpText = u""
 
     if blockHelpText:
-        blockHelpText = """<span class="help-block">%(blockHelpText)s</span>""" % locals(
+        blockHelpText = u"""<span class="help-block">%(blockHelpText)s</span>""" % locals(
         )
     else:
-        blockHelpText = ""
+        blockHelpText = u""
 
     if not focusedInputText:
-        focusedInputText = ""
-        focusId = ""
+        focusedInputText = u""
+        focusId = u""
     else:
-        focusedInputText = """value="%(focusedInputText)s" """ % locals()
-        focusId = "focusedInput "
+        focusedInputText = u"""value="%(focusedInputText)s" """ % locals()
+        focusId = u"focusedInput "
 
     if required:
-        required = """required"""
+        required = u"""required"""
     else:
-        required = ""
+        required = u""
 
     if disabled:
-        disabled = """disabled"""
-        disabledId = ""
+        disabled = u"""disabled"""
+        disabledId = u""
     else:
-        disabled = ""
-        disabledId = ""
+        disabled = u""
+        disabledId = u""
 
     if defaultValue:
-        if isinstance(defaultValue, str):
+        if isinstance(defaultValue, str) or isinstance(defaultValue, unicode):
             defaultValue = defaultValue.replace('"', "\"")
-            defaultValue = '"%(defaultValue)s"' % locals()
-        defaultValue = """value=%(defaultValue)s """ % locals()
-    else:
-        defaultValue = ""
+            defaultValue = u'"%(defaultValue)s"' % locals()
 
-    thisInput = """
+        defaultValue = u"""value=%(defaultValue)s """ % locals()
+    else:
+        defaultValue = u""
+
+    thisInput = u"""
     %(prependContent)s
     <input class="%(searchClass)s %(span)s" id="%(htmlId)s%(focusId)s%(disabledId)s" %(focusedInputText)s type="%(ttype)s" %(step)s placeholder="%(placeholder)s" %(required)s %(disabled)s name="%(htmlId)s" %(defaultValue)s>
     %(appendContent)s
     """ % locals()
 
     if rightText is not False:
-        thisInput = """<label class="inline">%(thisInput)s%(rightText)s </label>""" % locals(
+        thisInput = u"""<label class="inline">%(thisInput)s%(rightText)s </label>""" % locals(
         )
 
-    formInput = """
+    formInput = u"""
         <div class="%(prependClass)s %(appendClass)s %(hidden)s %(pull)s">
             
             %(thisInput)s 
